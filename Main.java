3437ebe78f022225cd8d0f45a2fb72d0e7fe386c
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-16 14:19:30 -0700

    initial state

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+
+
+        //System.out.println(sol.mergeTwoLists(list1, list2));
+
+    }
+}
