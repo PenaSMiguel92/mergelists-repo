d2bb6bb6372005be034f3785ed33c3bdf2d43bee
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-16 14:27:14 -0700

    generateList method added to main

--- Main.java
+++ Main.java
@@ -2,9 +2,17 @@ public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-
-
         //System.out.println(sol.mergeTwoLists(list1, list2));
 
+    }
+    
+    public static ListNode generateList(int[] values) {
+        ListNode list = new ListNode(-1, null);
+        ListNode listPointer = list;
+        for (int val : values) {
+            listPointer.next = new ListNode(val, null);
+            listPointer = listPointer.next;
+        }
+        return list;
     }
 }
