a4737b008a640a6a6c10c21e4556d1e07fd6d563
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-16 14:34:46 -0700

    testing code added

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-
-        //System.out.println(sol.mergeTwoLists(list1, list2));
+        ListNode list1 = generateList(new int[] { 1, 2, 4 });
+        ListNode list2 = generateList(new int[] { 1, 3, 4 });
+        ListNode list3 = generateList(new int[] {});
+        ListNode list4 = generateList(new int[] {});
+        ListNode list5 = generateList(new int[] {});
+        ListNode list6 = generateList(new int[] { 0 });
+        System.out.println(printList(sol.mergeTwoLists(list1, list2)));
+        System.out.println(printList(sol.mergeTwoLists(list3, list4)));
+        System.out.println(printList(sol.mergeTwoLists(list5, list6)));
 
     }
     
@@ -13,6 +20,16 @@ public class Main {
             listPointer.next = new ListNode(val, null);
             listPointer = listPointer.next;
         }
-        return list;
+        return list.next;
+    }
+
+    public static String printList(ListNode list) {
+        String val = "[";
+        while(list!=null) {
+            val += list.val + ", ";
+            list = list.next;
+        }
+        val += "]";
+        return val;
     }
 }
